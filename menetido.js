afe397dd8767c847658c8a53b8d4c374be2ed94c
nuke7 <gombos.marton@icloud.com>
2020-11-18 21:06:24 +0100

    added hev files, solved js

---
+++ menetido.js
@@ -0,0 +1,19 @@
+let gomb = document.querySelector("#menetido");
+
+function calculateMenetido() {
+  let oda = document.querySelector("#odaut").value;
+  let vissza = document.querySelector("#visszaut").value;
+  let percek = Math.abs(vissza - oda);
+  return percek;
+}
+
+function menetidoPercben() {
+  let perc = calculateMenetido();
+  if (perc === 0) {
+    alert("Ugyanott vagy te idióta!");
+  } else {
+    alert(`A menetidő: ${perc}`);
+  }
+}
+
+gomb.addEventListener("click", menetidoPercben);
