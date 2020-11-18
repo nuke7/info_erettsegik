eb003c7b6899dd9c95bd42d6b4f3c3117063320b
kecsmark <kecskemeti456@gmail.com>
2020-11-18 21:35:08 +0100

    Hello Dani vagyok a Hévröl, aki....

--- menetido.js
+++ menetido.js
@@ -1,19 +1,28 @@
 let gomb = document.querySelector("#menetido");
+let gom2 = document.querySelector("#Dani")
 
 function calculateMenetido() {
-  let oda = document.querySelector("#odaut").value;
-  let vissza = document.querySelector("#visszaut").value;
-  let percek = Math.abs(vissza - oda);
-  return percek;
+    let oda = document.querySelector("#odaut").value;
+    let vissza = document.querySelector("#visszaut").value;
+    let percek = Math.abs(vissza - oda);
+    return percek;
 }
 
 function menetidoPercben() {
-  let perc = calculateMenetido();
-  if (perc === 0) {
-    alert("Ugyanott vagy te idióta!");
-  } else {
-    alert(`A menetidő: ${perc}`);
-  }
+    let perc = calculateMenetido();
+    if (perc === 0) {
+        alert("Ugyanott vagy te idióta!");
+    } else {
+        alert(`A menetidő: ${perc}`);
+    }
+}
+
+
+
+function loadpadge() {
+    window.location = "index2.html"
+
 }
 
 gomb.addEventListener("click", menetidoPercben);
+gomb2.addEventListener("click", loadpadge)
